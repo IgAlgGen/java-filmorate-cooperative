217dd894466cf16fc36b973efc9b869f68b4d6d3
IgAlGen <genvarskiy.i.a@gmail.com>
2025-09-10 15:41:26 +0500

    created db

--- src/main/java/ru/yandex/practicum/filmorate/model/MpaRating.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/MpaRating.java
@@ -1,5 +1,26 @@
 package ru.yandex.practicum.filmorate.model;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum MpaRating {
-    G, PG, PG_13, R, NC_17
+    G(1, "G"),
+    PG(2, "PG"),
+    PG_13(3, "PG-13"),
+    R(4, "R"),
+    NC_17(5, "NC-17");
+
+    private final int id;
+    private final String title;
+
+    public static MpaRating fromId(int id) {
+        for (MpaRating rating : values()) {
+            if (rating.id == id) {
+                return rating;
+            }
+        }
+        throw new IllegalArgumentException("Неизвестный MPA id: " + id);
+    }
 }
