4191e579bc3feef1fc41ebbed01261528cda9179
IgAlGen <genvarskiy.i.a@gmail.com>
2025-10-18 17:36:10 +0500

    Создание контроллера и сервиса для Режисера CRUD only

--- src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/DirectorController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Director;
 import ru.yandex.practicum.filmorate.service.DirectorService;
 
+import java.net.URI;
 import java.util.List;
 
 @Slf4j
@@ -20,27 +21,37 @@ public class DirectorController {
 
     @PostMapping
     public ResponseEntity<Director> create (@Valid @RequestBody Director director){
-        return null;
+        log.info("Добавление режиссера: {}", director.toString());
+        Director created = directorService.create(director);
+        URI location = URI.create("/directors/" + created.getId());
+        return ResponseEntity.created(location).body(created);
     }
 
     @PutMapping
     public ResponseEntity<Director> update (@Valid @RequestBody Director director){
-        return null;
+        log.info("Обновление режиссера с ID {}: {}", director.getId(), director.toString());
+        Director updated = directorService.update(director);
+        return ResponseEntity.ok(updated);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Director> getById (int id){
-        return null;
+        log.info("Получение режиссера с ID {}", id);
+        return ResponseEntity.ok(directorService.getById(id));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete (int id){
-        return null;
+        log.info("Удаление режиссера с ID {}", id);
+        directorService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping
     public ResponseEntity<List<Director>> getAll (){
-        return null;
+        log.info("Получение списка всех режиссеров");
+        List<Director> directors = directorService.getAll();
+        return ResponseEntity.ok(directors);
     }
 
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/DirectorService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/DirectorService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.model.Director;
+import ru.yandex.practicum.filmorate.storage.director.DirectorStorage;
 
 import java.util.List;
 
@@ -11,22 +12,25 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class DirectorService {
+    private final DirectorStorage directorStorage;
+
     public Director create(Director director) {
-        return null;
+        return directorStorage.create(director);
     }
 
     public Director update(Director director) {
-        return null;
+        return directorStorage.update(director);
     }
 
     public Director getById(int id) {
-        return null;
+       return directorStorage.getById(id).orElseThrow();
     }
 
     public void delete(int id) {
+        directorStorage.deleteById(id);
     }
 
     public List<Director> getAll() {
-        return null;
+        return directorStorage.getAll();
     }
 }
